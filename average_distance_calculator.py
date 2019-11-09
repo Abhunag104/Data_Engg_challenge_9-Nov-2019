735db21791bedfa4ac67c01cb62220d263a6ba2d
Abhunag104 <33800234+Abhunag104@users.noreply.github.com>
2019-11-10 01:28:11 +0530

    Add files via upload

---
+++ average_distance_calculator.py
@@ -0,0 +1,96 @@
+import argparse
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.sql.window import Window
+
+
+def event_filter(spark, input_path):
+    event_df = spark.read.format("json").load(input_path + "/*/*.txt")
+
+    event_df.show(truncate=False)
+    filtered_df = event_df.filter(f.col('event').
+                                  isin(['update', 'register', 'deregister']))
+    co_ordinates_df = filtered_df.select(f.col('at').alias('time'),
+                                         f.col('data.id').alias('id'),
+                                         f.struct(f.col('data.location.lat').alias('lat'),
+                                                  f.col('data.location.lng').alias('lng'))
+                                         .alias('coordinate'))
+
+    return co_ordinates_df
+
+
+def average_distance(co_ordinates_df):
+
+    window = Window.partitionBy(f.col('id')).orderBy(f.col('time'))
+    sorted_df = co_ordinates_df.withColumn('lat_lng_list',
+                                           f.collect_list("coordinate").over(window))
+    sorted_df = sorted_df.groupBy(f.col('id')) \
+        .agg(f.max('lat_lng_list').alias('lat_lng_list'))
+
+    from pyspark.sql.functions import udf
+
+    avg_udf = udf(calculate_average)
+
+    result_df = sorted_df.withColumn('avg_dist',
+                                     avg_udf(f.column('lat_lng_list')))
+
+    result_df = result_df.select('id', 'avg_dist')
+
+    return result_df
+
+
+def _write_results(result_df, output_path):
+
+    df = (result_df.repartition(1)
+          .write.mode('overwrite')
+          .format('csv'))
+
+    df.save(output_path)
+
+
+def calculate_average(lat_lng_list):
+
+    my_list = []
+    for i in lat_lng_list:
+        my_list.append(tuple(i))
+
+    import geopy.distance
+    avg_dist = []
+    for dist_pair in range(0, len(my_list) - 1):
+        if all(my_list[dist_pair]):
+            co_ordinate_1 = my_list[dist_pair]
+            co_ordinate_2 = my_list[dist_pair + 1]
+
+            avg_dist.append(geopy.distance.vincenty(co_ordinate_1, co_ordinate_2).km)
+
+    return sum(avg_dist)/len(avg_dist)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Spark job to calculate average distance of sensor data'
+    )
+
+    parser.add_argument(
+        '--input-path', action='store', type=str,
+        dest='input_path', required=True,
+        help='input path for the data'
+    )
+
+    parser.add_argument(
+        '--output-path', action='store', type=str,
+        dest='output_path', required=True,
+        help='output path for the clean data'
+    )
+
+    args = parser.parse_args()
+    app_name = 'door2door Average calculation job'
+    spark = SparkSession.builder \
+        .appName(app_name) \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+    event_df = event_filter(spark, input_path=args.input_path)
+    average_df = average_distance(co_ordinates_df = event_df)
+    _write_results(result_df=average_df, output_path=args.output_path)
