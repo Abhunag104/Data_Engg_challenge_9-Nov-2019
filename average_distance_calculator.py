23e85514b84551d545bddc7670b2da5f6fa96dd0
Abhunag104 <33800234+Abhunag104@users.noreply.github.com>
2019-11-10 12:16:26 +0530

    Add files via upload

--- average_distance_calculator.py
+++ average_distance_calculator.py
@@ -6,8 +6,6 @@ from pyspark.sql.window import Window
 
 def event_filter(spark, input_path):
     event_df = spark.read.format("json").load(input_path + "/*/*.txt")
-
-    event_df.show(truncate=False)
     filtered_df = event_df.filter(f.col('event').
                                   isin(['update', 'register', 'deregister']))
     co_ordinates_df = filtered_df.select(f.col('at').alias('time'),
@@ -21,6 +19,7 @@ def event_filter(spark, input_path):
 
 def average_distance(co_ordinates_df):
 
+    # sorting done on basis of time provided and related lat, lng captured
     window = Window.partitionBy(f.col('id')).orderBy(f.col('time'))
     sorted_df = co_ordinates_df.withColumn('lat_lng_list',
                                            f.collect_list("coordinate").over(window))
@@ -29,6 +28,7 @@ def average_distance(co_ordinates_df):
 
     from pyspark.sql.functions import udf
 
+    # Udf is used to pass list of lat, lng values and get average distance
     avg_udf = udf(calculate_average)
 
     result_df = sorted_df.withColumn('avg_dist',
@@ -48,6 +48,7 @@ def _write_results(result_df, output_path):
     df.save(output_path)
 
 
+# calculation of distance by Latitide,Longitude in km
 def calculate_average(lat_lng_list):
 
     my_list = []
@@ -57,11 +58,12 @@ def calculate_average(lat_lng_list):
     import geopy.distance
     avg_dist = []
     for dist_pair in range(0, len(my_list) - 1):
-        if all(my_list[dist_pair]):
+        if all(my_list[dist_pair]):  # avoid empty tuple
             co_ordinate_1 = my_list[dist_pair]
             co_ordinate_2 = my_list[dist_pair + 1]
 
-            avg_dist.append(geopy.distance.vincenty(co_ordinate_1, co_ordinate_2).km)
+            avg_dist.append(geopy.distance.vincenty(co_ordinate_1,
+                                                    co_ordinate_2).km)
 
     return sum(avg_dist)/len(avg_dist)
 
@@ -92,5 +94,5 @@ if __name__ == '__main__':
 
     spark.sparkContext.setLogLevel("WARN")
     event_df = event_filter(spark, input_path=args.input_path)
-    average_df = average_distance(co_ordinates_df = event_df)
+    average_df = average_distance(co_ordinates_df=event_df)
     _write_results(result_df=average_df, output_path=args.output_path)
